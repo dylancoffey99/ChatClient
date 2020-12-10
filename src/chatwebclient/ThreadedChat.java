022bbfd21564b15b366e98c199d655f9f1aeb398
dylancoffey99 <dylancoffey99@gmail.com>
2020-12-10 21:54:56 +0000

    TextArea updates messages correctly

--- src/chatwebclient/ThreadedChat.java
+++ src/chatwebclient/ThreadedChat.java
@@ -7,59 +7,52 @@ import java.util.List;
 import java.util.ArrayList;
 import javax.swing.JTextArea;
 
-public class ThreadedChat extends Thread implements Runnable {
+public class ThreadedChat extends Thread {
     private ChatWebService proxy;
     private int uid, sid;
     private JTextArea messageArea;   
-    private String message, reciever;
-    private List<Message> messages = new ArrayList<>(); 
-    private List<Message> messagesCopy = new ArrayList<>(); 
+    private String message, reciever, lastMessage;
+    private List<Message> messages = new ArrayList<>();  
     
     public ThreadedChat(ChatWebService p, int u, int s, String r) {     
         proxy = p;
         uid = u;
         sid = s;
         reciever = r;
-      
+        
         messageArea = new JTextArea(16,47);
-        messagesCopy = proxy.getMessages(uid, sid, reciever, 0);
-        messages = proxy.getMessages(uid, sid, reciever, 0);
-        updateTextArea();
     }
     
-    public void run() {        
+    @Override
+    public void run() {   
+        messages = proxy.getMessages(uid, sid, reciever, 0);
+        lastMessage = messages.get(messages.size()-1).getMessage();
+        fillTextArea();
+        
         while(true) {
             messages = proxy.getMessages(uid, sid, reciever, 0);
+            message = messages.get(messages.size()-1).getMessage();
             
-            if(equalLists(messages, messagesCopy)) {    
-                updateTextArea();
-                messagesCopy = proxy.getMessages(uid, sid, reciever, 0); 
-                
-                try {           
-                    sleep(3000);
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
+            if (!message.equals(lastMessage)) {
+                messageArea.append(message + "\n");
+                lastMessage = messages.get(messages.size()-1).getMessage();
+            }
+            try {
+                sleep(3000);
+            } 
+            catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
     
-    public void updateTextArea() {
+    public void fillTextArea() {       
         for (int i = 0; i < messages.size(); i++) {
             message = messages.get(i).getMessage();
             messageArea.append(message + "\n");
         }
     }
-    
-    public boolean equalLists(List<Message> list, List<Message> listCopy) {     
-        if(listCopy.containsAll(list)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
+
     public JTextArea getMessageArea() {
         return messageArea;
     }
